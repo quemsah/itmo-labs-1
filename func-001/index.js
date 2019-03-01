25e05f23356cded47183412d542fb76bdec5f9ab
Евгений <quemsaurose@gmail.com>
2019-03-01 21:36:45 +0300

    /

--- func-001/index.js
+++ func-001/index.js
@@ -1,6 +1,7 @@
 
 const concat = require('goss_concat');
-function func(r,g,b) {
+function func(r=255,g=255,b=255) 
+{
 console.log(concat('rgb(',r,',',g,',',b,')'));
 }
-func(255,255,255);
+func();
