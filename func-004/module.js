6a44d0c6468d6eb7c1421cf5d81695003c6dd238
Евгений <quemsaurose@gmail.com>
2019-03-02 02:11:33 +0300

    /

---
+++ func-004/module.js
@@ -0,0 +1,5 @@
+/*export default (...funcs) => funcs.reduce((par1, par2) => (...args) => par1(par2(...args), x => x));*/
+const aaa = 5;
+export default aaa;
+                      
+document.getElementById('answer').innerHTML += aaa;   
