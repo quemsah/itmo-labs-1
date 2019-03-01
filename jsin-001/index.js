f7ddbf65b6feb7f2e9117c3f0a7f45a20e366659
Евгений <quemsaurose@gmail.com>
2019-03-01 14:27:34 +0300

    /

--- jsin-001/index.js
+++ jsin-001/index.js
@@ -7,15 +7,15 @@
     request.open('GET', 'https://www.cbr-xml-daily.ru/daily_json.js');
     request.onload = function(e) {
         //cDeg
-        document.write("cDeg = " + (fDeg* 1.8 + 32) +"<br>");
+        document.getElementById('answer').innerHTML += ("cDeg = " + (fDeg* 1.8 + 32) +"<br>");
         //dollars
         kurs = JSON.parse(this.responseText).Valute.USD.Value;
         dollars = roubles/kurs;
-        document.write("dollars = " + dollars.toFixed(2)+"<br>");
+        document.getElementById('answer').innerHTML += ("dollars = " + dollars.toFixed(2)+"<br>");
         //x1 x2
         let x1 = (-b+Math.sqrt(b*b-4*a*c))/(2*a);
         let x2 = (-b-Math.sqrt(b*b-4*a*c))/(2*a);
-        document.write("x1 = " + x1 +"<br>");
-        document.write("x2 = " + x2 +"<br>");
+        document.getElementById('answer').innerHTML += ("x1 = " + x1 +"<br>");
+        document.getElementById('answer').innerHTML += ("x2 = " + x2 +"<br>");
         }
     request.send(null);
