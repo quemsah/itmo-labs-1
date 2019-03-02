127ebc2ffcdeccbc863eb9e9e2db9018a3579d67
Евгений <quemsaurose@gmail.com>
2019-03-03 00:50:55 +0300

    все-то не работает

--- func-004/index.js
+++ func-004/index.js
@@ -1,5 +1,12 @@
 import smth from './module.js'
-
-document.getElementById('answer').innerHTML += smth(1,1);
+     const qv = x => x * x;
+     const sc = x => x + 1;
+document.getElementById('answer').innerHTML += 'qv(sc(5)) = ' + qv(sc(5)) + '<br>';
+document.getElementById('answer').innerHTML += 'sc(qv(5)) = ' + sc(qv(5)) + '<br>';
+document.getElementById('answer').innerHTML += smth(qv(5)) + '<br>';
+document.getElementById('answer').innerHTML += smth(sc(5)) + '<br>';
+document.getElementById('answer').innerHTML += smth(qv(sc(5))) + '<br>';
+document.getElementById('answer').innerHTML += smth(sc(qv(5))) + '<br>';
+document.getElementById('answer').innerHTML += smth + '<br>';
 
 
--- func-004/module.js
+++ func-004/module.js
@@ -1,9 +1,10 @@
-/*export default (...funcs) => funcs.reduce((par1, par2) => (...args) => par1(par2(...args), x => x));*/
+/*export default (...funcs) => funcs.reduce((accum, fn) => (...args) => accum(fn(...args), x => x));*/
 export default function itworks(...funcs) {
     return funcs.reduce(
         function itworkstoo(par1, par2) {
-        	return (...args) => par1(par2(...args), x => x);
-    /*        	return function ihopeitworks(...args) {    }*/
+        	function ihopeitworks(...args) {
+        		return par1(par2(...args), x => x);
+        	}
         }
-    	);
+    );
 }
