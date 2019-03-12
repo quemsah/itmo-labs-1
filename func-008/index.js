6c27924580b94cd16aed5e1a131c2bf58713e425
Евгений <quemsaurose@gmail.com>
2019-03-12 14:52:50 +0300

    добавил задания func-006,007,008

--- func-008/index.js
+++ func-008/index.js
@@ -10,57 +10,59 @@ fetchResult.then(response => response.text())
 		})
 		.then(response => response.text())
 		.then((x) => data = JSON.parse(x));
-	/*const request = new XMLHttpRequest();
-	request.open('POST', 'https://3336.kodaktor.ru/funcpsst', false);
-	request.send();
-	let data = JSON.parse(request.responseText);*/
-	document.getElementById('answer').innerHTML += (data.g[0] + "<br>");
-	document.getElementById('answer').innerHTML += (data.g[1] + "<br>");
+	const mappingFunc = x => parseInt(x.value);
+	const f0_inputs = Array.from(document.querySelectorAll(".f0")).map(mappingFunc);
+	const f1_inputs = Array.from(document.querySelectorAll(".f1")).map(mappingFunc);
+	const functions = data.g.map(
+		function (elem) {
+			return new Function('x', elem);
+		}
+	);
+	console.log(functions[0](f0_inputs));
+	console.log(functions[1](f1_inputs));
+})();
+/* это работает но не совсем по заданию		
+            var answer = data.g.map(function (item, index) {
+			eval("function f" + index + "_func(x) {" + item + "}");
+			eval("console.log(f" + index + "_func(f" + index + "_inputs));")
+		});
+*/
 
-	const mappingFunction = x => parseInt(x.value);
-
-	const f0_inputs = Array.from(document.querySelectorAll(".f0")).map(mappingFunction);
-	const f1_inputs = Array.from(document.querySelectorAll(".f1")).map(mappingFunction);
-	console.log(f0_inputs);
-	console.log(f1_inputs);
-
-	eval("function f0_func(x) {" + data.g[0] + "}");
+/* тож работает
+    eval("function f0_func(x) {" + data.g[0] + "}");
 	eval("function f1_func(x) {" + data.g[1] + "}");
 	console.log(f0_func(f0_inputs));
 	console.log(f1_func(f1_inputs));
-})();
-/*
-Ранее - https://kodaktor.ru/zzzzzzz
-
-Пожалуйста, сделайте следующее:
-
-1. Отправьте POST-запрос на адрес 3336.kodaktor.ru/funcpsst
-2. Вы получите массив текстов тел функций
-3. Получите по нему с помощью метода map массив функций, каждая из которых способна получить сколько угодно аргументов
-4. Подсчитайте значение первой функции от значений этих текстовых полей:
-
-<input value="3" class="f0"><input value="4" class="f0"><input value="5" class="f0"><input value="10" class="f0">
-
-5. Подсчитайте значение второй функции от значений этих текстовых полей: 
-
-<input value="1" class="f1"><input value="2" class="f1"><input value="3" class="f1"><input value="4" class="f1"><input value="5" class="f1"><input value="6" class="f1"><input value="7" class="f1"><input value="8" class="f1"><input value="9" class="f1"><input value="10" class="f1">
-
-  
-ПОЯСНЕНИЕ
-
-Деструктурируйте полученный от сервера ответ в переменную g (массив тел)
-С помощью метода map сопоставьте элементам массива g функции
-(для этого используйте вызов конструктора Function, первым аргументом которого будет оператор rest/spread от x, а вторым каждое тело)
-
-С помощью метода Array.from превратите результат работы querySelectorAll
-в массив
-С помощью метода map теперь сопоставьте элементам этого массива (т.е. инпутам)
-их значения, преобразованные в числа с помощью + или Number
-
-С помощью метода apply примените полученные массивы к вашим функциям
-(f[0].apply(...))
-
-* Реализуйте интерфейс вида
-<img src="interface800x200.gif" alt="">
-для добавления произвольного количества аргументов
 */
+/// вторая часть
+const mappingFunction = x => parseInt(x.value);
+const bubbleSort = arr => {
+	for (let i = 0, endI = arr.length - 1; i < endI; i++) {
+		let wasSwap = false;
+		for (let j = 0, endJ = endI - i; j < endJ; j++) {
+			if (arr[j] > arr[j + 1]) {
+				[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+				wasSwap = true;
+			}
+		}
+		if (!wasSwap) break;
+	}
+	return arr;
+};
+$("#btnClicker").on("click", function () {
+	let number_of_inputs = $('#demo-input').val();
+	for (i = 0; i < number_of_inputs; i++) {
+		$("#myDiv").append("<input type='number' id='sort-input' class='sort-input' step='1'>");
+	}
+	$('.col-12-xsmall').remove();
+	$('#btnClicker').remove();
+	$("#myDiv").append("<button id='btnSort'>Сортируй</button>")
+	$("#btnSort").on("click", function () {
+		const user_inputs = Array.from($(".sort-input")).map(mappingFunction);
+		let sorted_inputs = bubbleSort(user_inputs);
+		console.log("sorted_inputs", sorted_inputs);
+		$('.sort-input').each(function (index) {
+			$(this).val(sorted_inputs[index]);
+		});
+	});
+});
