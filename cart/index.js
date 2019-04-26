7974f8d4f70dde5c329cc4d2b6b04a8c33f540c1
Евгений <quemsaurose@gmail.com>
2019-04-26 03:58:20 +0300

    cart start

--- cart/index.js
+++ cart/index.js
@@ -1,32 +1,30 @@
-
 const getData = async (url) => (await fetch(url)).json();
-const setAttributes = (el, attrs) => {
-  for (var key in attrs)(key != "text") ? el.setAttribute(key, attrs[key]) : el.textContent = attrs[key]
-  return el;
-}
 
 const url = "https://kodaktor.ru/cart_data.json";
-
-let answer = document.querySelector('div.answer');
-let budgetButton = answer.appendChild(setAttributes(document.createElement("button"), {
-  "class": "button primary fit small",
-  "style": "width: 30%",
-  "text": "Нажми на меня"
-}));
-console.log(budgetButton);
-
-budgetButton.addEventListener("click", () => {
-  budgetButton.remove();
-});
 
 (async () => {
   let data = await getData(url);
   console.log(...[data]);
 
-  // Object.keys(data).forEach(((key) => {
+  let budgetButton = document.querySelector('button');
+  let budgetInput = document.querySelector('input');
+  let budgetH3 = document.querySelector('h3');
 
-  // }));
+  console.log(budgetButton);
 
+  budgetButton.addEventListener("click", () => {
+    console.log(budgetInput.value);
+    budgetButton.remove();
+    budgetInput.remove();
+    budgetH3.remove();
+  });
 
+  let ist = document.querySelector('#item');
+  console.log(ist)
+  let pri = document.querySelector('#cart');
+  console.log(pri);
+  ist.addEventListener('dragstart', e => e.dataTransfer.setData('text/plain', e.target.textContent));
+  pri.addEventListener('dragover', e => e.preventDefault());
+  pri.addEventListener('drop', e => e.target.textContent += e.dataTransfer.getData('text/plain'));
 
 })();
